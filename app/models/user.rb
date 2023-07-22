3311cecdde9cc300f43cf51f001a10e2d927477d
Wei Jie <jackchong398@gmail.com>
2023-07-23 00:04:18 +0800

    feat: remove primary key sequence on destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   has_many :messages, dependent: :destroy
-  
+  after_destroy :reset_pk_sequence
+
   enum status: { active: 1, inactive: 0}
+
+  private 
+    def reset_pk_sequence
+      User.connection.execute("DELETE FROM sqlite_sequence WHERE name = 'users';")
+    end
 end
